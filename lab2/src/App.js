beb34282cb5bef4f84d7208700d322e899d7d06a
TienDat5604 <tiendat562004@gmail.com>
2025-06-02 09:38:38 +0700

    Update lab2 content

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -42,19 +42,22 @@ function App() {
         </div>
       </nav>
 
-      {/* Phần giới thiệu */}
-      <section id="home" className="hero-section text-white text-center position-relative">
-        <div className="container">
-          <div className="hero-content">
-            <img src="/pizza1.jpg" alt="Neapolitan Pizza" className="hero-image" />
-            <h1 className="display-4">Neapolitan Pizza</h1>
+     <div className="hero-banner position-relative">
+        <div className="carousel-item active">
+          <img
+            src="/pizza1.jpg"
+            className="d-block w-100 hero-img"
+            alt="Neapolitan Pizza"
+          />
+          <div className="carousel-caption text-center">
+            <h2 className="display-4 fw-bold">Neapolitan Pizza</h2>
             <p className="lead">
-              If you are looking for a traditional Italian pizza, the Neapolitan is the best option!
+              If you are looking for a traditional Italian pizza, the Neapolitan
+              is the best option!
             </p>
-
           </div>
         </div>
-      </section>
+      </div>
 
       {/* Phần thực đơn */}
       <section id="menu" className="py-5 bg-dark text-white">
